31abc171ce78f577c9a8e4c3941e295675db8047
evansmbithi <evholmbithy@gmail..com>
2022-09-27 01:59:31 +0300

    Bill Manager feature completion

--- billmanager.js
+++ billmanager.js
@@ -1,3 +1,11 @@
 import net_pay from "./taxcalculator.js";
 
 console.log(net_pay);
+
+let netSalary = document.getElementById("net_salary");
+netSalary.value = net_pay;
+
+// get bill elements by id
+// parse them into integers
+// get the total bill amount
+// balance = net - total bill amount
