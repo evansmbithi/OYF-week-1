bebc18fa335f2f100f8628f3cbab2012f55cd4d8
evansmbithi <evholmbithy@gmail..com>
2022-09-27 10:40:24 +0300

    Debug complete

--- billmanager.js
+++ billmanager.js
@@ -5,23 +5,22 @@ import net_pay from "./taxcalculator.js";
 let netSalary = parseInt(document.getElementById("net_salary").value);
 netSalary = net_pay;
 
-function calculate(){ 
-    document.getElementById("total")
-      .addEventListener("click", () => {
-        const output = document.getElementById("output");
-        output.value = + output.value;
+function calculate() {
+  document.getElementById("total").addEventListener("click", () => {
+    const output = document.getElementById("output");
+    output.value = +output.value;
 
-    let totalSum = parseInt(document.getElementByName("qty").value)
+    let totalSum = parseInt(document.getElementsByName("qty").value);
     console.log(`totalSum: ${totalSum}`);
 
-    var total= 0;
-    for(var i=0; i < totalSum.length; i++){
-        if(parseInt(totalSum[i].value)){
-            total += parseInt(totalSum[i].value);
+    var total = 0;
+    for (var i = 0; i < totalSum.length; i++) {
+      if (parseInt(totalSum[i].value)) {
+        total += parseInt(totalSum[i].value);
+      }
     }
- }
-    document.getElementsByClassName('total').value = total;
+    document.getElementsByClassName("total").value = total;
     let bal = netSalary - totalSum;
     console.log(bal);
-   }
- }
+  });
+}
