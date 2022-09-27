b271f03f9ff69b45f5d9c04147e2e422cbf906fa
Awuo-Nancy <anyang@gmail.com>
2022-09-27 10:34:58 +0300

    completed bil manager logic

--- billmanager.js
+++ billmanager.js
@@ -1,11 +1,27 @@
 import net_pay from "./taxcalculator.js";
 
-console.log(net_pay);
+// console.log(net_pay);
 
-let netSalary = document.getElementById("net_salary");
-netSalary.value = net_pay;
+let netSalary = parseInt(document.getElementById("net_salary").value);
+netSalary = net_pay;
 
-// get bill elements by id
-// parse them into integers
-// get the total bill amount
-// balance = net - total bill amount
+function calculate(){ 
+    document.getElementById("total")
+      .addEventListener("click", () => {
+        const output = document.getElementById("output");
+        output.value = + output.value;
+
+    let totalSum = parseInt(document.getElementByName("qty").value)
+    console.log(`totalSum: ${totalSum}`);
+
+    var total= 0;
+    for(var i=0; i < totalSum.length; i++){
+        if(parseInt(totalSum[i].value)){
+            total += parseInt(totalSum[i].value);
+    }
+ }
+    document.getElementsByClassName('total').value = total;
+    let bal = netSalary - totalSum;
+    console.log(bal);
+   }
+ }
