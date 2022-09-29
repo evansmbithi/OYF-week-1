6d5f44914f4da7e952f3bf15ae65ccf9351d87c2
evansmbithi <evholmbithy@gmail..com>
2022-09-29 14:36:11 +0300

    Added default bill items

--- billmanager.js
+++ billmanager.js
@@ -2,39 +2,61 @@ let net_pay = document.getElementById("netpay").value;
 
 let bill_content = document.querySelector(".bill-content");
 
-let travel = parseInt(document.getElementById("travel"));
-let rent = parseInt(document.getElementById("rent"));
-let entertainment = parseInt(document.getElementById("entertainment"));
-let food = parseInt(document.getElementById("food"));
-let shopping = parseInt(document.getElementById("shopping"));
-
-var bill_amount = [];
+// var bill_amount = [];
+const bills = {
+  rent: 2000,
+  transport: 1000,
+  entertainment: 500,
+  food: 2000,
+};
 
 // generate bills
-function grabBill() {
-  let bill_label = document.getElementById("bill-label").value;
-  let bill_value = document.getElementById("bill-amount").value;
-
+function grabBill(key, value) {
   let content = `
     <tr>
-      <td>${bill_label}</td>
-      <td>${bill_value}</td>
+      <td>${key}</td>
+      <td>${value}</td>
     </tr>
   `;
+
   return content;
 }
+
+// display bills in a table
+let table_content = document.createElement("table");
+table_content.id = "initial-table";
+for (const [key, value] of Object.entries(bills)) {
+  table_content.innerHTML += grabBill(key, value);
+}
+bill_content.appendChild(table_content);
+
+// replace table upon new bill entry
+function newBill() {
+  const initialTbl = bill_content.children[0]; // first child of bill-content
+
+  let table_content = document.createElement("table");
+  for (const [key, value] of Object.entries(bills)) {
+    table_content.innerHTML += grabBill(key, value);
+  }
+  bill_content.replaceChild(table_content, initialTbl);
+}
+
+// display balance
 
 // onclick add Bill
 let addBill = document.getElementById("add-bill");
 addBill.addEventListener("click", () => {
-  bill_amount.push(parseInt(document.getElementById("bill-amount").value));
-  let table_content = document.createElement("table");
-  table_content.innerHTML = grabBill();
-  bill_content.appendChild(table_content);
+  // add bill item
+  let bill_label = document.getElementById("bill-label").value;
+  let bill_value = document.getElementById("bill-amount").value;
+  bills[bill_label] = bill_value;
+  newBill();
+
+  // compute new balance
+
+  // clear bill input fields
   document.getElementById("bill-label").value = "";
   document.getElementById("bill-amount").value = "";
-
-  document.getElementById("balance").value = computeBal(); //update balance
 });
 
 // compute balance
