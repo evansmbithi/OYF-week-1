a59f8653945edb94055d94d470bde037961f2189
evansmbithi <evholmbithy@gmail..com>
2022-10-11 01:56:34 +0300

    added MYSQL image to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,24 @@
-FROM nginx:latest
-COPY . /usr/share/nginx/html/
+#FROM nginx:latest
+#COPY . /usr/share/nginx/html/
+
+#FROM httpd
+#COPY . /usr/local/apache2/htdocs/
+
+###### RUN MYSQL IN A DOCKER CONTAINER ######
+
+FROM mysql:latest
+
+ENV MYSQL_ROOT_PASSWORD=go-getters
+
+COPY ./db.sql /docker-entrypoint-initdb.d/
+
+# docker build -t mysql-grp7 .
+# docker run --name group7 -dp 3307:3306 mysql-grp7
+# docker exec -it group7 bash # opens up a bash terminal
+# bash-4.0# mysql -u root -p
+# password: go-getters
+# opens up mysql>
+# you can now execute SQL scripts
+# mysql> SHOW DATABASES;
+# SHOW TABLES;
+# SELECT * FROM tbl_name;
---
+++ db.sql
@@ -0,0 +1,12 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (1, 'Jakie', 'Hundey', 'jhundey0@prnewswire.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (2, 'Nikita', 'Faucett', 'nfaucett1@buzzfeed.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (3, 'Mathias', 'Gavozzi', 'mgavozzi2@nature.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (4, 'Davida', 'Skaife d''Ingerthorpe', 'dskaifedingerthorpe3@cdc.gov', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (5, 'Thaxter', 'Batte', 'tbatte4@trellian.com', 'Male');
